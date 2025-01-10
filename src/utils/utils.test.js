da876110bba559f809aa08e64bec522ee31258d1
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-01-09 21:14:17 -0300

    feat(CalculaNovoSaldo): Adicionado teste de transação

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,3 +1,29 @@
+import { calculaNovoSaldo } from "./index";
+
+describe("Quando realizo uma transação", () => {
+  it("de depósito, o saldo deve aumentar", () => {
+    const transacao = {
+      transacao: "Depósito",
+      valor: 50,
+    };
+
+    const novoSaldo = calculaNovoSaldo(transacao, 100);
+
+    expect(novoSaldo).toBe(150);
+  });
+
+  it("de transferência, o saldo deve diminuir", () => {
+    const transacao = {
+      transacao: "Transferência",
+      valor: 50,
+    };
+
+    const novoSaldo = calculaNovoSaldo(transacao, 100);
+
+    expect(novoSaldo).toBe(50);
+  });
+});
+
 it("Deve calcular e retornar o rendimento do saldo", () => {
   const calculaRendimento = jest.fn((saldo) => saldo + saldo * 0.005);
 
